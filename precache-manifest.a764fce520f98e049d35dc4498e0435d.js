7446cfd192a5eaa7323aa72d01899de5624e6eeb
Ruslan Vasylyshyn <r.y.vasylyshyn@gmail.com>
2021-04-19 02:15:04 +0300

    Updates

---
+++ precache-manifest.a764fce520f98e049d35dc4498e0435d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d4a7545f115a91f473abb6dfd94355",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4bccebeff0f33a10d838",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "3ed3ebde95252fd8335c",
+    "url": "./static/js/2.06028b1a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.06028b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bccebeff0f33a10d838",
+    "url": "./static/js/main.45f71d9a.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
